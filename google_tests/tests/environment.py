151c429add7e4aae2a54a942dbceaccca4abfe7b
Marek Martyniak <ms.martyniak@gmail.com>
2018-11-02 00:05:54 +0100

    Zaktualizowano środowisko testowe

--- google_tests/tests/environment.py
+++ google_tests/tests/environment.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 
 def before_scenario(context, scenario):
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument('window-size=1600x900')
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1600x900')
     
     driver = webdriver.Chrome(chrome_options=options)
     context.driver = driver
